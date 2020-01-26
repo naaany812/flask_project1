daa804ef444f7d426ef04e02ed8efc92484f9f54
Anna Kukhta <Anna_Kukhta@dell.com>
2020-01-26 12:31:48 +0300

    Add base.html and update tour template and route

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template  # подключаем render_template, который включает функции для Jinja
+from flask import Flask, render_template
 import data
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html', tours = data.tours)  # рендерим шаблон, передавая переменные
+    return render_template('index.html', 
+    title = data.title,
+    subtitle = data.subtitle,
+    description = data.description,
+    departures = data.departures,
+    tours = data.tours) 
 
 
 @app.route("/from/<direction>")
@@ -16,12 +21,20 @@ def from_direction(direction):
             direction_tours[tour_id] = tour
 
     direction_tours.values()
-    return render_template('direction.html', direction = data.departures.get(direction), tours = direction_tours)
+    return render_template('direction.html', 
+    title = data.title,
+    direction = data.departures.get(direction),
+    departures = data.departures, 
+    tours = direction_tours)
 
 
-@app.route("/tours/<tour_id>")
-def tours(tour_id):
-    return render_template('tour.html', tour=tour_id)
+@app.route("/tours/<id>/")
+def tours(id):
+    tour = data.tours.get(int(id))
+    return render_template('tour.html',
+    title = data.title,
+    departures = data.departures,
+    tour = tour)
 
 
 app.run(port=8000)
