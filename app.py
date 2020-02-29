8ce14bf0f9a63c75ade4050fb662351c305e19aa
Anna Kukhta <Anna_Kukhta@dell.com>
2020-02-29 22:37:30 +0300

    Add env in gitignore and update the app for flake8

--- app.py
+++ app.py
@@ -3,39 +3,43 @@ import data
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-    return render_template('index.html', 
-    title = data.title,
-    subtitle = data.subtitle,
-    description = data.description,
-    departures = data.departures,
-    tours = data.tours) 
+    return render_template(
+        'index.html',
+        title=data.title,
+        subtitle=data.subtitle,
+        description=data.description,
+        departures=data.departures,
+        tours=data.tours)
 
 
 @app.route("/from/<direction>")
 def from_direction(direction):
     direction_tours = dict()
-    for tour_id,tour in data.tours.items():
+    for tour_id, tour in data.tours.items():
         if tour.get("departure") == direction:
             direction_tours[tour_id] = tour
 
     direction_tours.values()
-    return render_template('direction.html', 
-    title = data.title,
-    direction = data.departures.get(direction),
-    departures = data.departures, 
-    tours = direction_tours)
+    return render_template(
+        'direction.html',
+        title=data.title,
+        direction=data.departures.get(direction),
+        departures=data.departures,
+        tours=direction_tours)
 
 
 @app.route("/tours/<id>/")
 def tours(id):
     tour = data.tours.get(int(id))
-    return render_template('tour.html',
-    title = data.title,
-    departures = data.departures,
-    tour = tour)
+    return render_template(
+        'tour.html',
+        title=data.title,
+        departures=data.departures,
+        tour=tour)
+
 
 if __name__ == '__main__':
     app.run(port=8000)
-
